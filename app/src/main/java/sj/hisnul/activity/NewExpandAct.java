3374f5586c2e443f5849b76221b31c0b5683ac54
salahja <salahuddinjaweed@protonmail.com>
2022-11-26 17:02:58 +0530

    second commit prference list modified to Bottomsheet

--- app/src/main/java/sj/hisnul/activity/NewExpandAct.java
+++ app/src/main/java/sj/hisnul/activity/NewExpandAct.java
@@ -8,13 +8,11 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.ExpandableListView;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -25,6 +23,7 @@ import com.example.utility.QuranGrammarApplication;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Objects;
 
 import sj.hisnul.entity.hduanames;
 import sj.hisnul.fragments.HDuaNamesfrag;
@@ -35,10 +34,9 @@ public class NewExpandAct extends BaseActivity implements SearchView.OnQueryText
     private int previousGroup = -1;
     private HashMap<String, ParentItem> subjects;
     private ArrayList<ParentItem> parentItemsList;
-    private HashMap<Integer, boolean[]> mChildCheckStates;
+
     private ExpandableListView expandableListView;
     private ArrayList<ChildItem> childItemsList;
-    private Toolbar toolbar;
     private ArrayList<hduanames> hduanamesArrayList = new ArrayList<>();
     private CustomAdapter customAdapter;
 
@@ -47,24 +45,15 @@ public class NewExpandAct extends BaseActivity implements SearchView.OnQueryText
         super.onCreate(savedInstanceState);
         setContentView(R.layout.expand_act);
         //     setContentView(R.layout.activity_dua_group);
-        toolbar = findViewById(R.id.my_action_bar);
+        Toolbar toolbar = findViewById(R.id.my_action_bar);
         // setSupportActionBar(toolbar);
         setSupportActionBar(toolbar);
         ActionBar actionbar = getSupportActionBar();
-        final int color = ContextCompat.getColor(this, R.color.color_background_overlay);
-        final int colorsurface = ContextCompat.getColor(this, R.color.pink_skin);
-        final int coloronbackground = ContextCompat.getColor(this, R.color.neutral0);
         SharedPreferences shared = PreferenceManager.getDefaultSharedPreferences(QuranGrammarApplication.getContext());
-        String isNightmode = shared.getString("theme", "dark");
-   /*     if (isNightmode.equals("dark") || isNightmode.equals("blue")) {
-            toolbar.setBackgroundColor(coloronbackground);
-            toolbar.setBackgroundColor(color);
-        } else {
-            toolbar.setBackgroundColor(colorsurface);
-        }*/
         //  actionbar.setDisplayHomeAsUpEnabled(true);
         //  actionbar.setDisplayShowHomeEnabled(true);
         //  setSupportActionBar(toolbar);
+        assert actionbar != null;
         actionbar.setDisplayHomeAsUpEnabled(true);
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
@@ -91,9 +80,7 @@ public class NewExpandAct extends BaseActivity implements SearchView.OnQueryText
     }
 
     private void loadDatas() {
-        String header = "";
         for (hduanames hduanames : hduanamesArrayList) {
-            header = hduanames.getChapname();
             addItem(hduanames.getChapname(), hduanames.getDuaname(), hduanames.getID(), hduanames.getChap_id());
 
         }
@@ -101,15 +88,15 @@ public class NewExpandAct extends BaseActivity implements SearchView.OnQueryText
 
     }
 
-    private int addItem(String parentItemName, String childItemName, String id, int chapterid) {
+    @SuppressWarnings("UnusedAssignment")
+    private void addItem(String parentItemName, String childItemName, String id, int chapterid) {
         int size = parentItemsList.size();
-        int groupPosition = 0;
+        int groupPosition;
         if (parentItemName.isEmpty()) {
             parentItemName = parentItemsList.get(size - 1).getName();
         }
         //check the hash map if the group already exists
         ParentItem parentItemObj = subjects.get(parentItemName);
-        ParentItem parentItemObj2 = subjects.get(chapterid);
         //add the group if doesn't exists
         if (parentItemObj == null) {
             parentItemObj = new ParentItem();
@@ -133,62 +120,51 @@ public class NewExpandAct extends BaseActivity implements SearchView.OnQueryText
         parentItemObj.setChildList(childItemsList);
         //find the group position inside the list
         groupPosition = parentItemsList.indexOf(parentItemObj);
-        return groupPosition;
     }
 
     private void displayList() {
         //  loadData();
         customAdapter = new CustomAdapter(this, parentItemsList);
         expandableListView.setAdapter(customAdapter);
-        expandableListView.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
-            @Override
-            public boolean onGroupClick(ExpandableListView parent, View v, int groupPosition, long id) {
-                if (expandableListView.isGroupExpanded(groupPosition)) {
-                    expandableListView.collapseGroup(groupPosition);
-                    previousGroup = -1;
-                } else {
-                    expandableListView.expandGroup(groupPosition);
-                    if (previousGroup != -1) {
-                        expandableListView.collapseGroup(previousGroup);
-                    }
-                    previousGroup = groupPosition;
+        expandableListView.setOnGroupClickListener((parent, v, groupPosition, id) -> {
+            if (expandableListView.isGroupExpanded(groupPosition)) {
+                expandableListView.collapseGroup(groupPosition);
+                previousGroup = -1;
+            } else {
+                expandableListView.expandGroup(groupPosition);
+                if (previousGroup != -1) {
+                    expandableListView.collapseGroup(previousGroup);
                 }
-                return true;
+                previousGroup = groupPosition;
             }
+            return true;
         });
-        expandableListView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
-            @Override
-            public void onGroupExpand(int groupPosition) {
-                /* Auto Scrolling */
-                expandableListView.setSelectedGroup(groupPosition);
-            }
+        expandableListView.setOnGroupExpandListener(groupPosition -> {
+            /* Auto Scrolling */
+            expandableListView.setSelectedGroup(groupPosition);
         });
-        expandableListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
-            @Override
-            public boolean onChildClick(ExpandableListView parent, View v,
-                                        int groupPosition, int childPosition, long id) {
-                int chap_id = parentItemsList.get(groupPosition).getChapterid();
-                getSupportActionBar().hide();
-                Bundle bundle1 = new Bundle();
-                bundle1.putInt("chap_id", chap_id);
-                bundle1.putBoolean("cattwo", false);
-                Fragment fragvsi = HDuaNamesfrag.newInstance();
-                fragvsi.setArguments(bundle1);
-                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction()
-                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-                transaction.replace(R.id.frame_container, fragvsi, "items");
-                //     transaction.addToBackStack("setting");
-                transaction.addToBackStack("items");
-                transaction.commit();
-                return false;
-            }
+        expandableListView.setOnChildClickListener((parent, v, groupPosition, childPosition, id) -> {
+            int chap_id = parentItemsList.get(groupPosition).getChapterid();
+            Objects.requireNonNull(getSupportActionBar()).hide();
+            Bundle bundle1 = new Bundle();
+            bundle1.putInt("chap_id", chap_id);
+            bundle1.putBoolean("cattwo", false);
+            Fragment fragvsi = HDuaNamesfrag.newInstance();
+            fragvsi.setArguments(bundle1);
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction()
+                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+            transaction.replace(R.id.frame_container, fragvsi, "items");
+            //     transaction.addToBackStack("setting");
+            transaction.addToBackStack("items");
+            transaction.commit();
+            return false;
         });
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        getSupportActionBar().show();
+        Objects.requireNonNull(getSupportActionBar()).show();
     }
 
 
@@ -204,11 +180,16 @@ public class NewExpandAct extends BaseActivity implements SearchView.OnQueryText
    */
 
     @Override
+    public void onBackPressed() {
+
+    }
+
+    @SuppressWarnings("UnusedAssignment")
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_search, menu);
         SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
         SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
-        View actionView = menu.findItem(R.id.search).getActionView();
         searchView = (SearchView) menu.findItem(R.id.search).getActionView();
         searchView.setSearchableInfo
                 (searchManager.getSearchableInfo(getComponentName()));
